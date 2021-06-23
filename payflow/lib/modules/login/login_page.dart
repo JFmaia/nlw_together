68d3095dd8b6d2c19b59f03f1141e668397aac0f
JFmaia <jfmaia741@gmail.com>
2021-06-23 15:20:33 -0300

    modified file
    Modificando o codigo do login_page para ter o controller de login_controller.

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -13,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controlle = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -68,19 +69,8 @@ class _LoginPageState extends State<LoginPage> {
                       top: 40,
                     ),
                     child: SocialLoginButton(
-                      ontap: () async {
-                        GoogleSignIn _googleSignIn = GoogleSignIn(
-                          scopes: [
-                            'email',
-                            'https://www.googleapis.com/auth/contacts.readonly',
-                          ],
-                        );
-                        try {
-                          final response = await _googleSignIn.signIn();
-                          print(response);
-                        } catch (error) {
-                          print(error);
-                        }
+                      ontap: () {
+                        controlle.googleSignIn(context);
                       },
                     ),
                   ),
