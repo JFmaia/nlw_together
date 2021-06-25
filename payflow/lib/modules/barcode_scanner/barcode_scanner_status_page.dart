eb21d502d629b0719206da090225fc0d111d672d
JFmaia <jfmaia741@gmail.com>
2021-06-25 16:11:37 -0300

    modified file
    Tirando o cameraController, pois não faz sentido estar nos status, isso não peformatico, isso como já diz 'controller' fica no controller kkk.

--- payflow/lib/modules/barcode_scanner/barcode_scanner_status_page.dart
+++ payflow/lib/modules/barcode_scanner/barcode_scanner_status_page.dart
@@ -1,27 +1,30 @@
-import 'package:camera/camera.dart';
-
 class BarcodeScannerStatus {
   final bool isCameraAvalible;
   final String error;
   final String barcode;
-  CameraController? cameraController;
+  final bool stopScanner;
 
   BarcodeScannerStatus({
+    this.stopScanner = false,
     this.isCameraAvalible = false,
-    this.cameraController,
     this.error = "",
     this.barcode = "",
   });
 
-  factory BarcodeScannerStatus.available(CameraController controller) =>
-      BarcodeScannerStatus(
-          isCameraAvalible: true, cameraController: controller);
+  factory BarcodeScannerStatus.available() => BarcodeScannerStatus(
+        isCameraAvalible: true,
+        stopScanner: false,
+      );
 
-  factory BarcodeScannerStatus.error(String message) =>
-      BarcodeScannerStatus(error: message);
+  factory BarcodeScannerStatus.error(String message) => BarcodeScannerStatus(
+        error: message,
+        stopScanner: true,
+      );
 
-  factory BarcodeScannerStatus.barcode(String barcode) =>
-      BarcodeScannerStatus(barcode: barcode);
+  factory BarcodeScannerStatus.barcode(String barcode) => BarcodeScannerStatus(
+        barcode: barcode,
+        stopScanner: true,
+      );
 
   bool get showCamera => isCameraAvalible && error.isEmpty;
 
